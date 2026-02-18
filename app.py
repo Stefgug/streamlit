6442bec8037614b8271cc5f683b43abff573e012
Stéphane Guren <52997130+Stefgug@users.noreply.github.com>
2026-02-18 22:21:59 +0100

    Change header text in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
+st.markdown("""# Hello
 ## This is a sub header
 This is text.
 Je suis dans le cloud""")
